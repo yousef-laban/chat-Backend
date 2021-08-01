df15b3dfaf041162d9c6b91734a2864c6ea8ec60
Yousef-laban <yousef.labn@fgmail.com>
2021-08-01 11:20:23 +0300

    editGroupControllers

--- controllers/groupControllers.js
+++ controllers/groupControllers.js
@@ -1,8 +1,22 @@
-const { Group, Group_User } = require("../db/models");
+const { Group, Group_User, User } = require("../db/models");
 
 exports.fetchAllGroups = async (req, res, next) => {
   try {
-    const allGroups = await Group.findAll();
+    const allGroups = await Group.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+      include: [
+        {
+          model: User,
+          as: "users",
+          attributes: ["id", "username"],
+          through: {
+            model: Group_User,
+          },
+        },
+      ],
+    });
     res.json(allGroups);
   } catch (error) {
     next(error);
@@ -20,7 +34,12 @@ exports.groupCreat = async (req, res, next) => {
 
     await Group_User.bulkCreate(groupUser);
 
-    res.status(201).json(groupUser);
+    const finalForm = {
+      ...newgroup.toJSON(),
+      users: req.body.usersId,
+    };
+
+    res.status(201).json(finalForm);
   } catch (error) {
     next(error);
   }
